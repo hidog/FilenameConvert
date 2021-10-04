e248f4a5e8d1bed7b14e894b983f13dc970e3acf
hidog <hidog.gao@gmail.com>
2021-10-04 09:28:34 +0800

    add code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,44 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+#include <QFileDialog>
+
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    init();
 }
+
+
+
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+
+
+void MainWindow::init()
+{
+    connect(    ui->selectFolderButton,    SIGNAL(clicked()),      this,       SLOT(selet_folder_slot())   );
+}
+
+
+
+
+void MainWindow::selet_folder_slot()
+{
+    QString dirname = QFileDialog::getExistingDirectory( this, tr("select dir"), "D:\\", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks );
+    ui->selectFolderButton->setDisabled(true);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,10 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
+
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -15,7 +19,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void selet_folder_slot();
+
 private:
     Ui::MainWindow *ui;
+
+    void init();
+
 };
+
+
+
+
 #endif // MAINWINDOW_H
