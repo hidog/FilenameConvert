4b0462d64bfc0d1b62b14396b01e8f2f03ff1993
hidog <hidog.gao@gmail.com>
2024-11-29 22:40:49 +0800

    update

--- worker.cpp
+++ worker.cpp
@@ -269,7 +269,7 @@ Mode    Worker::get_mode()
 
 
 
-#if 0 // load sub
+#if 1    // load sub
 void    Worker::rename( QString src, QString dst )
 {
     QDir    src_dir(src);
@@ -286,10 +286,10 @@ void    Worker::rename( QString src, QString dst )
     //for( auto& info : list )
     for( int i = 0; i < list.size(); i += 2 )
     {
-        auto info = list.at(i + 0);  // sometimes need exchange with sub
+        auto info = list.at(i + 1);  // sometimes need exchange with sub
         auto qstr = info.fileName();
 
-        auto sub = list.at(i + 1);
+        auto sub = list.at(i + 0);
         auto sub_str = sub.fileName();
     
         utf8_tc_str     =   conv->Convert( qstr.toStdString().c_str() );
@@ -297,16 +297,16 @@ void    Worker::rename( QString src, QString dst )
         output_str      =   utf8_tc_str.substr( 0, utf8_tc_str.size() - 4 ) + ".mkv";
 
         // 10 bit
-        fprintf( fp, "ffmpeg -i \"%s\" -i \"%s\" -map 0:0 -map 0:1 -map 1:0 -vcodec hevc_nvenc -cq 25 -pix_fmt p010le -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
-            utf8_tc_str.c_str(), utf8_sub_str.c_str(), output_str.c_str() );
+        fprintf( fp, "ffmpeg -i \"%s\" -i \"%s\" -map 0:0 -map 0:1 -map 1:0 -vcodec hevc_nvenc -cq 24 -pix_fmt p010le -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
+                    utf8_tc_str.c_str(), utf8_sub_str.c_str(), output_str.c_str() );
         // 8 bit
-        //fprintf( fp, "ffmpeg -i \"%s\" -i \"%s\" -map 0:0 -map 0:1 -map 1:0 -vcodec hevc_nvenc -cq 25 -pix_fmt yuv420p -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
-          //       utf8_tc_str.c_str(), utf8_sub_str.c_str(), utf8_tc_str.c_str() );
+        //fprintf( fp, "ffmpeg -i \"%s\" -i \"%s\" -map 0:0 -map 0:1 -map 1:0 -vcodec hevc_nvenc -cq 24 -pix_fmt yuv420p -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
+          //          utf8_tc_str.c_str(), utf8_sub_str.c_str(), output_str.c_str() );
     }
 
     fclose(fp);
 }
-#elif 1 // load sub, not intersperse
+#elif 0 // load sub, not intersperse
 void    Worker::rename( QString src, QString dst )
 {
     QDir    src_dir(src);
@@ -364,11 +364,11 @@ void    Worker::rename( QString src, QString dst )
     
         utf8_tc_str     =   conv->Convert( qstr.toStdString().c_str() );
         // 10 bit
-        fprintf( fp, "ffmpeg -i \"%s\" -map 0:0 -map 0:1 -map 0:3 -vcodec hevc_nvenc -cq 25 -pix_fmt p010le -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
-                utf8_tc_str.c_str(), utf8_tc_str.c_str() );
+        fprintf( fp, "ffmpeg -i \"%s\" -map 0:0 -map 0:1 -map 0:2 -vcodec hevc_nvenc -cq 24 -pix_fmt p010le -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
+                   utf8_tc_str.c_str(), utf8_tc_str.c_str() );
         // 8 bit
-        //fprintf( fp, "ffmpeg -i \"%s\" -map 0:0 -map 0:1 -map 0:2 -vcodec hevc_nvenc -cq 30 -pix_fmt yuv420p -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
-        //         utf8_tc_str.c_str(), utf8_tc_str.c_str() );
+        //fprintf( fp, "ffmpeg -i \"%s\" -map 0:0 -map 0:1 -map 0:3 -vcodec hevc_nvenc -cq 24 -pix_fmt yuv420p -acodec copy -scodec copy -disposition:s:0 default \"./output/%s\"\n", 
+          //       utf8_tc_str.c_str(), utf8_tc_str.c_str() );
     }
 
     fclose(fp);
@@ -392,8 +392,12 @@ void    Worker::rename( QString src, QString dst )
         auto qstr = info.fileName();
     
         utf8_tc_str     =   conv->Convert( qstr.toStdString().c_str() );
-        fprintf( fp, "ffmpeg -i \"%s\" -vcodec hevc_nvenc -cq 30 -pix_fmt yuv420p -acodec copy \"./output/%s\"\n", 
-            utf8_tc_str.c_str(), utf8_tc_str.c_str() );
+        // 10 bit
+        //fprintf( fp, "ffmpeg -i \"%s\" -vcodec hevc_nvenc -cq 25 -pix_fmt p010le -acodec copy \"./output/%s\"\n", 
+          //          utf8_tc_str.c_str(), utf8_tc_str.c_str() );
+        // 8 bit
+        fprintf( fp, "ffmpeg -i \"%s\" -vcodec hevc_nvenc -cq 24 -pix_fmt yuv420p -acodec copy \"./output/%s\"\n", 
+                    utf8_tc_str.c_str(), utf8_tc_str.c_str() );
     }
 
     fclose(fp);
